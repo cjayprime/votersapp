85192b0f0113aa19d6383a006e236c42de25c4b1
Nna Chijioke E <nnachijioke@yahoo.com>
2019-07-25 15:02:15 +0100

    Global Commit

---
+++ android/app/src/main/java/com/votersapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.votersapp;
+
+import com.facebook.react.ReactActivity;
+import com.imagepicker.permissions.OnImagePickerPermissionsCallback; // <- add this import
+import com.facebook.react.modules.core.PermissionListener; // <- add this import
+
+public class MainActivity extends ReactActivity implements OnImagePickerPermissionsCallback {
+     private PermissionListener listener;
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "votersapp";
+    }
+    
+    @Override
+    public void setPermissionListener(PermissionListener listener)
+    {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        if (listener != null)
+        {
+        listener.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+}
